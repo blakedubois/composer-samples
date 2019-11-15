e3f17048174e68e6402828f719c34a449c8701b3
bsnisar <User16961>
2019-11-15 17:05:31 +0200

    example of autoscale option

--- dataproc-hive-workflow-template/README.md
+++ dataproc-hive-workflow-template/README.md
@@ -11,3 +11,15 @@
 - Copy dataset to GS bucket `gsutil cp records.tsv gs://<BUCKET>/dataset/`
 - Load sql `gsutil cp -r sql/* gs://<BUCKET>/sql/`
 - Load Workflow template `gcloud dataproc workflow-templates import wlt-demo-wf --source workflow.yml --region <REGION>`
+
+
+#### For autoscale example 
+To enable autoscaling on a cluster:
+- Create an [autoscaling policy](https://cloud.google.com/dataproc/docs/concepts/configuring-clusters/autoscaling#create_an_autoscaling_policy)
+```
+gcloud dataproc autoscaling-policies import <policy_id> \
+       --source=<policy-config-filename.yaml>
+```
+
+- refer autoscale config in workflow template in next format
+   `projects/[projectId]/locations/[region]/autoscalingPolicies/[policy_id]`
--- dataproc-hive-workflow-template/dags/simple_dataproc_workflow_inline.py
+++ dataproc-hive-workflow-template/dags/simple_dataproc_workflow_inline.py
@@ -93,3 +93,81 @@ with DAG(dag_id='simple_dataproc_workflow_inline',
     )
 
     load_data >> run_hive
+
+
+
+
+with DAG(dag_id='simple_dataproc_workflow_inline_autoscale',
+         default_args=default_args,
+         start_date=start_date,
+         schedule_interval=None) as dag:
+
+    run_hive = DataprocWorkflowTemplateInstantiateInlineOperator(
+        task_id='RunHiveWorkflow',
+        project_id=PROJECT_ID,
+        region=REGION_ID,
+        template={
+            "jobs": [
+                {
+                    "hiveJob": {
+                        "queryFileUri": f"gs://{BUCKET_ID}/sql/1_load-data-job.sql",
+                        "scriptVariables": {
+                            "INPUT_PATH": f"gs://{BUCKET_ID}/dataset/records.tsv"
+                        }
+                    },
+                    "stepId": "1_load-data-job"
+                },
+                {
+                    "hiveJob": {
+                        "queryFileUri": f"gs://{BUCKET_ID}/sql/2_read-data-job.sql",
+                        "scriptVariables": {
+                            "OUTPUT_PATH": f"gs://{BUCKET_ID}"
+                        }
+                    },
+                    "prerequisiteStepIds": [
+                        "1_load-data-job"
+                    ],
+                    "stepId": "2_read-data-job"
+                }
+            ],
+            "placement": {
+                "managedCluster": {
+                    "clusterName": "two-node-cluster",
+                    "config": {
+                        "gceClusterConfig": {
+                            "zoneUri": "us-central1-f"
+                        },
+                        "masterConfig": {
+                            "diskConfig": {
+                                "bootDiskSizeGb": 250
+                            },
+                            "machineTypeUri": "n1-standard-2"
+                        },
+                        "softwareConfig": {
+                            "imageVersion": "1.3-deb9"
+                        },
+                        "workerConfig": {
+                            "diskConfig": {
+                                "bootDiskSizeGb": 250
+                            },
+                            "machineTypeUri": "n1-standard-2",
+                            "numInstances": 2,
+
+                            #
+                            # Autoscale reference
+                            #
+                            "autoscalingConfig": {
+                                "policyUri": "projects/wmt-data-search/locations/us-central1/autoscalingPolicies/wmt-scale-policy_01"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    )
+
+    load_data = DummyOperator(
+        task_id='DummyTask'
+    )
+
+    load_data >> run_hive
